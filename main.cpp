d29f1522d2e9644486d4534596be143d2fda406c
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-29 18:59:18 +0300

    main

--- main.cpp
+++ main.cpp
@@ -26,15 +26,56 @@ int main() {
         {{4, 7, 2025}, "Parking", 350}          //arr[1]
     }};
 
-    Driver d2;
-    d2.fillDriver(); //arr[0]
-    d2.addViolationToDriver(); //arr[1]
-    d2.addViolationToDriver(); //arr[2]
+    Driver d2 = {"Shevchenko Taras", "AX543212", 1, new Violation[1] {
+        {{12, 4, 2026}, "Sign", 300}
+    }};
+    // d2.fillDriver(); //arr[0]
+    // d2.addViolationToDriver(); //arr[1]
+    // d2.addViolationToDriver(); //arr[2]
+    //
+    // cout << "----------------------------------------------------\n";
+    //
+    // d1.showDriver();
+    // d2.showDriver();
 
-    cout << "----------------------------------------------------\n";
 
-    d1.showDriver();
-    d2.showDriver();
+    //main program
+    int NDrivers = 0;
+    Driver* arrDrivers = nullptr;
+
+    addItemBack(arrDrivers, NDrivers, d1);
+    addItemBack(arrDrivers, NDrivers, d2);
+
+    int menu = 0;
+    char text[100] = "";
+
+    do {
+        cout << "==================================================\n";
+        cout << "\t MENU:\n";
+        cout << "==================================================\n";
+        cout << "0. Exit\n";
+        cout << "1. Show all drivers\n";
+        cout << "2. Add a new driver\n";
+        cout << "3. Show info by number of the car\n";
+        cout << "4. Add a new violation by number of the car\n";
+        cout << "==================================================\n";
+        cout << ">>> "; cin >> menu;
+
+        switch (menu) {
+            case 1: {
+                for (int i =0; i < NDrivers; i++) {
+                    cout << "#" << i+1 << ". " << arrDrivers[i].name << " " << arrDrivers[i].carNumber << endl;
+                }
+            }break;
+            case 2: {
+                Driver tmp;
+                cin.ignore();
+                tmp.fillDriver(); //getline(name, 100);
+                addItemBack(arrDrivers, NDrivers, tmp);
+            }break;
+        }
+
+    } while (menu!=0);
 
     return 0;
 }

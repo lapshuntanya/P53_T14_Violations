970676f1913be5f9878a09d5a3f8b4a524c2f3f6
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-29 18:35:07 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "DateMac.h"
 #include "Violation.h"
+#include "Driver.h"
 int main() {
 
     // Date d1;
@@ -11,14 +12,29 @@ int main() {
     // d2.printDate();
 
 
-    Violation v1 = {{1, 1, 2000}, "Speed", 2000};
-    Violation v2;
-    v2.fillViolation();
-    cout << "---------------\n";
+    // Violation v1 = {{1, 1, 2000}, "Speed", 2000};
+    // Violation v2;
+    // v2.fillViolation();
+    // cout << "---------------\n";
+    // v1.showViolation();
+    // v2.showViolation();
 
-    v1.showViolation();
-    v2.showViolation();
 
+
+    Driver d1 = {"Pupkin Mykola", "AX1234KA", 2, new Violation[2] {
+        {{1, 1, 2000}, "Speed", 2000},          //arr[0]
+        {{4, 7, 2025}, "Parking", 350}          //arr[1]
+    }};
+
+    Driver d2;
+    d2.fillDriver(); //arr[0]
+    d2.addViolationToDriver(); //arr[1]
+    d2.addViolationToDriver(); //arr[2]
+
+    cout << "----------------------------------------------------\n";
+
+    d1.showDriver();
+    d2.showDriver();
 
     return 0;
 }
